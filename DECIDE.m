850527ecd6d125aa429165158a075038e5307333
velmaidlu <75670344+velmaidlu@users.noreply.github.com>
2021-03-10 19:56:30 +0100

    Added LIC 12

--- DECIDE.m
+++ DECIDE.m
@@ -257,6 +257,30 @@ if (NUMPOINTS>=3)
 end
 end
 
+%% LIC 12
+function out = lic12(POINTS, NUMPOINTS, PARAMETERS);
+out1 = 0;
+out2 = 0;
+out = 0;
+k_pts = PARAMETERS.K_PTS;
+len1 = PARAMETERS.LENGTH1;
+len2 = PARAMETERS.LENGTH2;
+    if NUMPOINTS < 3
+        return
+    end
+    for i = 1:(NUMPOINTS - (k_pts + 1))
+        p1 = POINTS(i, :);
+        p2 = POINTS(i + k_pts + 1, :);
+        if norm(p2 - p1) > len1 
+            out1 = 1;
+        end
+        if norm(p2 - p1) < len2
+            out2 = 1;
+        end
+    end
+out = out1*out2;
+end
+
 %% LIC 13
 function out = lic13(POINTS,NUMPOINTS,A_PTS,B_PTS,RADIUS1,RADIUS2)
 out=0;

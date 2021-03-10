b8246503421c6a034c67a5e88cd40421bd57d7ba
velmaidlu <75670344+velmaidlu@users.noreply.github.com>
2021-03-10 19:31:28 +0100

    Added LIC 10

--- DECIDE.m
+++ DECIDE.m
@@ -220,6 +220,30 @@ end
 
 end
 
+%% LIC 10
+function out = lic10(POINTS, NUMPOINTS, PARAMETERS);
+out = 0;
+e_pts = PARAMETERS.E_PTS;
+f_pts = PARAMETERS.F_PTS;
+a1 = PARAMETERS.AREA1;
+    if NUMPOINTS < 5
+        return
+    end
+    for i = 1:(NUMPOINTS - (e_pts + f_pts + 2))
+        p1 = POINTS(i, :);
+        p2 = POINTS(i + e_pts + 1, :);
+        p3 = POINTS(i + e_pts + f_pts + 2);
+        v1 = p2 - p1;
+        v2 = p3 - p1;
+        v1(3) = 0;
+        v2(3) = 0;
+        area = norm(cross(v1, v2))/2;
+        if area > a1
+            out = 1;
+        end
+    end
+end
+
 %% LIC 11
 function out = lic11(POINTS,NUMPOINTS,G_PTS)
 out=0;

efb4b16104fd0c6cd1d94c30255797fca56331e1
velmaidlu <75670344+velmaidlu@users.noreply.github.com>
2021-04-01 16:34:27 +0200

    Refactoring: Decompose Conditional

--- DECIDE.m
+++ DECIDE.m
@@ -71,10 +71,14 @@ min_distance = PARAMETERS.LENGTH1;
     for i = 1:(NUMPOINTS - 1)
         coordinate_1 = POINTS(i, :);
         coordinate_2 = POINTS(i + 1, :);
-        if (norm(coordinate_1 - coordinate_2) > min_distance)
+        if distance_check(coordinate_1, coordinate_2, min_distance)
             out = 1;
         end
     end
+end
+
+function check = distance_check(coordinate_1, coordinate_2, min_distance)
+    check = norm(coordinate_1 - coordinate_2) > min_distance;
 end
 
 %% LIC 1

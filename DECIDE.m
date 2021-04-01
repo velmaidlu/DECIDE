bed72e404ec6af15213efef016bcefe6174e9b68
velmaidlu <75670344+velmaidlu@users.noreply.github.com>
2021-04-01 16:14:20 +0200

    Refactoring: Rename Variable

--- DECIDE.m
+++ DECIDE.m
@@ -67,11 +67,11 @@ disp(LAUNCH);
 %% LIC 0
 function out = lic0(POINTS, NUMPOINTS, PARAMETERS)
 out = 0;
-len = PARAMETERS.LENGTH1;
+min_distance = PARAMETERS.LENGTH1;
     for i = 1:(NUMPOINTS - 1)
         v1 = POINTS(i, :);
         v2 = POINTS(i + 1, :);
-        if (norm(v1 - v2) > len)
+        if (norm(v1 - v2) > min_distance)
             out = 1;
         end
     end

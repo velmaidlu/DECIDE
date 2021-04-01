31d94234364aa0566c198f62aabcc09650e01497
velmaidlu <75670344+velmaidlu@users.noreply.github.com>
2021-04-01 17:26:55 +0200

    Refactoring: Decompose Conditional

--- test/lic2.m
+++ test/lic2.m
@@ -4,7 +4,7 @@ epsilon = PARAMETERS.EPSILON;
     for i = 1:(NUMPOINTS - 2)
         coordinate_difference_1 = POINTS(i, :) - POINTS(i + 1, :);
         coordinate_difference_2 = POINTS(i + 2, :) - POINTS(i + 1, :);
-        if (norm(coordinate_difference_1) == 0) || (norm(coordinate_difference_2) == 0) % Checks if the first or third points coincides with the second
+        if zero_vector_check(coordinate_difference_1, coordinate_difference_2) % Checks if the first or third points coincides with the second
             
         else
             angle = angle_between_vectors(coordinate_difference_1, coordinate_difference_2); % 0 <= Angle <= pi, so only one condition is enough
@@ -13,6 +13,10 @@ epsilon = PARAMETERS.EPSILON;
             end
         end
     end
+end
+
+function check = zero_vector_check(vector_1, vector_2)
+    check = (norm(vector_1) == 0) || (norm(vector_2) == 0);
 end
 
 function angle = angle_between_vectors(vector_1, vector_2)

c26cac24184d8e3b678f0e324b0a35f60248c682
velmaidlu <75670344+velmaidlu@users.noreply.github.com>
2021-04-01 17:04:18 +0200

    Refactoring: Rename Variable

--- test/lic2.m
+++ test/lic2.m
@@ -3,11 +3,11 @@ out = 0;
 epsilon = PARAMETERS.EPSILON;
     for i = 1:(NUMPOINTS - 2)
         coordinate_difference_1 = POINTS(i, :) - POINTS(i + 1, :);
-        v2 = POINTS(i + 2, :) - POINTS(i + 1, :);
-        if (norm(coordinate_difference_1) == 0) || (norm(v2) == 0) % Checks if the first or third points coincides with the second
+        coordinate_difference_2 = POINTS(i + 2, :) - POINTS(i + 1, :);
+        if (norm(coordinate_difference_1) == 0) || (norm(coordinate_difference_2) == 0) % Checks if the first or third points coincides with the second
             
         else
-            angle = abs(acos(dot(coordinate_difference_1, v2) / (norm(coordinate_difference_1) * norm(v2)))); % 0 <= Angle <= pi, so only one condition is enough
+            angle = abs(acos(dot(coordinate_difference_1, coordinate_difference_2) / (norm(coordinate_difference_1) * norm(coordinate_difference_2)))); % 0 <= Angle <= pi, so only one condition is enough
             if angle < (pi - epsilon)
                 out = 1;
             end

6de6423ac15798199dc9481bdb82cc44eb715b1d
velmaidlu <75670344+velmaidlu@users.noreply.github.com>
2021-04-01 16:18:10 +0200

    Refactoring: Rename Variable

--- test/lic0.m
+++ test/lic0.m
@@ -3,9 +3,9 @@ function out = lic0(POINTS, NUMPOINTS, PARAMETERS)
 out = 0;
 min_distance = PARAMETERS.LENGTH1;
     for i = 1:(NUMPOINTS - 1)
-        v1 = POINTS(i, :);
+        coordinate_1 = POINTS(i, :);
         v2 = POINTS(i + 1, :);
-        if (norm(v1 - v2) > min_distance)
+        if (norm(coordinate_1 - v2) > min_distance)
             out = 1;
         end
     end

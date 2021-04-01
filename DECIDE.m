595092418b63820ef230b02548d960cd6b807d5d
velmaidlu <75670344+velmaidlu@users.noreply.github.com>
2021-04-01 17:01:06 +0200

    Refactoring: Rename Variable

--- DECIDE.m
+++ DECIDE.m
@@ -106,12 +106,12 @@ function out = lic2(POINTS, NUMPOINTS, PARAMETERS)
 out = 0;
 epsilon = PARAMETERS.EPSILON;
     for i = 1:(NUMPOINTS - 2)
-        v1 = POINTS(i, :) - POINTS(i + 1, :);
+        coordinate_difference_1 = POINTS(i, :) - POINTS(i + 1, :);
         v2 = POINTS(i + 2, :) - POINTS(i + 1, :);
-        if (norm(v1) == 0) || (norm(v2) == 0) % Checks if the first or third points coincides with the second
+        if (norm(coordinate_difference_1) == 0) || (norm(v2) == 0) % Checks if the first or third points coincides with the second
             
         else
-            angle = abs(acos(dot(v1, v2) / (norm(v1) * norm(v2)))); % 0 <= Angle <= pi, so only one condition is enough
+            angle = abs(acos(dot(coordinate_difference_1, v2) / (norm(coordinate_difference_1) * norm(v2)))); % 0 <= Angle <= pi, so only one condition is enough
             if angle < (pi - epsilon)
                 out = 1;
             end

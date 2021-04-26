79957303c839873508e5f876fd8427d37b51a264
Hedieh Khosravi <hediyekhosravi@gmail.com.com>
2021-04-26 09:27:02 +0200

    Refactoring: Rename 2 variables

--- DECIDE.m
+++ DECIDE.m
@@ -271,13 +271,13 @@ out=0;
 if( NUMPOINTS>= 5) 
     for i= 1: (NUMPOINTS-2-C_PTS-D_PTS)
         % finding 2 vector corresponding to the 2nd point being vertex
-        V21=[POINTS(i+1+C_PTS,1)-POINTS(i,1) ; POINTS(i+1+C_PTS,2)-POINTS(i,2)];
-        V23=[POINTS(i+2+C_PTS+D_PTS,1)-POINTS(i+1+C_PTS,1); POINTS(i+2+C_PTS+D_PTS,2)-POINTS(i+1+C_PTS,2)];
+        vector_21=[POINTS(i+1+C_PTS,1)-POINTS(i,1) ; POINTS(i+1+C_PTS,2)-POINTS(i,2)];
+        vector_23=[POINTS(i+2+C_PTS+D_PTS,1)-POINTS(i+1+C_PTS,1); POINTS(i+2+C_PTS+D_PTS,2)-POINTS(i+1+C_PTS,2)];
         
         % finding the angle using dot product formula
         % V21.V23=|V21||V23|cos a
-        if (norm(V21) ~= 0 && norm(V23) ~= 0) % P1 or P3 should not coincide with the vertex P2
-            angle=acos(dot(V21,V23)/(norm(V21)*norm(V23)));
+        if (norm(vector_21) ~= 0 && norm(vector_23) ~= 0) % P1 or P3 should not coincide with the vertex P2
+            angle=acos(dot(vector_21,vector_23)/(norm(vector_21)*norm(vector_23)));
             if((angle < (pi-EPSILON)) || ((angle > (pi+EPSILON)) ))
                 out=1;
                 break;
